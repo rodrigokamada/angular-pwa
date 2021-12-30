a8ce51d3d34a6a0d039367322f90c3deabb596fa
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-12-30 14:06:07 -0300

    Adding the update version.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   isOnline: boolean;
+  modalVersion: boolean;
 
-  constructor() {
+  constructor(private swUpdate: SwUpdate) {
     this.isOnline = false;
+    this.modalVersion = true;
   }
 
   public ngOnInit(): void {
@@ -18,10 +22,29 @@ export class AppComponent implements OnInit {
 
     window.addEventListener('online',  this.updateOnlineStatus.bind(this));
     window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.pipe(
+        filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+        map((evt: any) => {
+          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`)
+          this.modalVersion = true;
+        }),
+      );
+    }
   }
 
   private updateOnlineStatus(): void {
     this.isOnline = window.navigator.onLine;
+  }
+
+  public updateVersion(): void {
+    this.modalVersion = false;
+    window.location.reload();
+  }
+
+  public closeVersion(): void {
+    this.modalVersion = false;
   }
 
 }
