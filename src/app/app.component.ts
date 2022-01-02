7c20449a0131528306fe4672ad9500b6536af9c1
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-01-02 15:32:30 -0300

    Adding the how to add the application code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
 import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { filter, map } from 'rxjs/operators';
 
@@ -11,8 +12,11 @@ export class AppComponent implements OnInit {
 
   isOnline: boolean;
   modalVersion: boolean;
+  modalPwaEvent: any;
+  modalPwaPlatform: string|undefined;
 
-  constructor(private swUpdate: SwUpdate) {
+  constructor(private platform: Platform,
+              private swUpdate: SwUpdate) {
     this.isOnline = false;
     this.modalVersion = false;
   }
@@ -27,15 +31,18 @@ export class AppComponent implements OnInit {
       this.swUpdate.versionUpdates.pipe(
         filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
         map((evt: any) => {
-          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`)
+          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`);
           this.modalVersion = true;
         }),
       );
     }
+
+    this.loadModalPwa();
   }
 
   private updateOnlineStatus(): void {
     this.isOnline = window.navigator.onLine;
+    console.info(`isOnline=[${this.isOnline}]`);
   }
 
   public updateVersion(): void {
@@ -45,6 +52,28 @@ export class AppComponent implements OnInit {
 
   public closeVersion(): void {
     this.modalVersion = false;
+  }
+
+  private loadModalPwa(): void {
+    if (this.platform.ANDROID) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault();
+        this.modalPwaEvent = event;
+        this.modalPwaPlatform = 'ANDROID';
+      });
+    }
+
+    if (this.platform.IOS && this.platform.SAFARI) {
+      const isInStandaloneMode = ('standalone' in window.navigator) && ((<any>window.navigator)['standalone']);
+      if (!isInStandaloneMode) {
+        this.modalPwaPlatform = 'IOS';
+      }
+    }
+  }
+
+  public addToHomeScreen(): void {
+    this.modalPwaEvent.prompt();
+    this.modalPwaPlatform = undefined;
   }
 
 }
