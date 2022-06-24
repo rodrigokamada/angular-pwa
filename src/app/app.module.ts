995287f977a9d2d12b83a71be8496641de7a4be3
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-06-24 09:41:46 -0300

    Improvements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
+      // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
